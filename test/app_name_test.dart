40d39f147f6b925169d5ba106f44ac08898b29a3
Rafael Amizes <r.amizes@gmail.com>
2023-11-12 16:55:47 -0300

    style: add stricter lint rules
    Closes #17

--- test/app_name_test.dart
+++ test/app_name_test.dart
@@ -1,5 +1,5 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:astronomy_picture_of_the_day/app_name.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   const name = 'Astronomy Picture Of The Day';
