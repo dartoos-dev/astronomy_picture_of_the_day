13b8b3a245101652959487a305f691a5afca839e
Rafael Amizes <r.amizes@gmail.com>
2023-11-21 21:09:29 -0300

    refactor: remove unused "appName" function
    Closes #38

--- test/app_name_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:astronomy_picture_of_the_day/app_name.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  const name = 'Astronomy Picture Of The Day';
-  test("The app name should be: '$name'.", () {
-    expect(appName, name);
-  });
-}
