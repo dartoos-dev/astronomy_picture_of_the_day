968b518297fc362071825aaaaa1aeffed07f4760
Rafael Amizes <r.amizes@gmail.com>
2023-11-28 10:38:11 -0300

    refactor: removal of 'isEquivalentTo' method
    Closes #48

--- lib/features/astronomy_picture_of_the_day/domain/entities/astronomy_picture.dart
+++ lib/features/astronomy_picture_of_the_day/domain/entities/astronomy_picture.dart
@@ -17,20 +17,17 @@ class AstronomyPicture extends Entity {
   final Uri mediumDefinitionUrl;
   final Uri highDefinitionUrl;
 
-  /// Compares this object with [other] by the properties [id], [date], [title],
-  /// [explanation], [mediumDefinitionUrl] and [highDefinitionUrl].
-  ///
-  /// Compatible with Liskov's Substitution Principle.
-  ///
-  /// For more details:
-  /// - [liskov-compatible-equals-hashcode](https://www.linkedin.com/pulse/liskov-compatible-equals-hashcode-doichin-iordanov/)
-  // @override
-  bool isEquivalentTo(AstronomyPicture other) {
-    return (id == other.id) &&
-        (date == other.date) &&
-        (mediumDefinitionUrl == other.mediumDefinitionUrl) &&
-        (highDefinitionUrl == other.highDefinitionUrl) &&
-        (title == other.title) &&
-        (explanation == other.explanation);
+  /// Equality by [id], [date], [title], [explanation], [mediumDefinitionUrl]
+  /// and [highDefinitionUrl].
+  @override
+  List<Object?> get props {
+    return [
+      super.props,
+      date,
+      title,
+      explanation,
+      mediumDefinitionUrl,
+      highDefinitionUrl,
+    ];
   }
 }
--- test/astronomy_picture_of_the_day/data/models/astronomy_picture_api_model_test.dart
+++ test/astronomy_picture_of_the_day/data/models/astronomy_picture_api_model_test.dart
@@ -3,46 +3,71 @@ import 'package:astronomy_picture_of_the_day/features/astronomy_picture_of_the_d
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  final date = DateTime.parse("2023-11-20");
-  const explanation = "This is a beatiful astronomy picture";
-  const title = "Astronomy Picture";
-  final highDefinitionUrl = Uri.parse(
-    "https://apod.nasa.gov/apod/image/2311/Horsehead_Hanson_2604.jpg",
-  );
+  const date = "2023-11-20";
+  const explanation = "Blah blah blah";
+  const hdurl =
+      "https://apod.nasa.gov/apod/image/2311/Horsehead_Hanson_2604.jpg";
+  const title = "The Horsehead Nebula";
+  const url = "https://apod.nasa.gov/apod/image/2311/Horsehead_Hanson_960.jpg";
 
-  final mediumDefinitionUrl = Uri.parse(
-    "https://apod.nasa.gov/apod/image/2311/Horsehead_Hanson_960.jpg",
-  );
-  final Map<String, dynamic> astronomyPictureJson = {
-    "date": date.toIso8601String(),
-    "title": title,
-    "explanation": explanation,
-    "hdurl": highDefinitionUrl.toString(),
-    "url": mediumDefinitionUrl.toString(),
-    "copyright": "NASA 2023",
-    "media_type": "image",
-    "service_version": "v1",
-  };
   group('constructor "fromJson"', () {
-    final model = AstronomyPictureApiModel.fromJson(astronomyPictureJson);
+    final Map<String, dynamic> astronomyPictureJson = {
+      "date": date,
+      "title": title,
+      "explanation": explanation,
+      "hdurl": hdurl,
+      "url": url,
+      "copyright": "NASA 2023",
+      "media_type": "image",
+      "service_version": "v1",
+    };
+    final modelFromJson =
+        AstronomyPictureApiModel.fromJson(astronomyPictureJson);
     test('should be a subclass of AstronomyPicture entity', () async {
-      expect(model, isA<AstronomyPicture>());
+      expect(modelFromJson, isA<AstronomyPicture>());
     });
-
     test('should extract "date" from the json object', () async {
-      expect(model.date, equals(date));
+      expect(modelFromJson.date, equals(DateTime.parse(date)));
     });
     test('should extract "title" from the json object', () async {
-      expect(model.title, equals(title));
+      expect(modelFromJson.title, equals(title));
     });
     test('should extract "explanation" from the json object', () async {
-      expect(model.explanation, equals(explanation));
+      expect(modelFromJson.explanation, equals(explanation));
     });
     test('should extract "hdurl" from the json object', () async {
-      expect(model.highDefinitionUrl, equals(highDefinitionUrl));
+      expect(modelFromJson.highDefinitionUrl, equals(Uri.parse(hdurl)));
     });
     test('should extract "url" from the json object', () async {
-      expect(model.mediumDefinitionUrl, equals(mediumDefinitionUrl));
+      expect(modelFromJson.mediumDefinitionUrl, equals(Uri.parse(url)));
+    });
+  });
+
+  group('constructor "fromStringData"', () {
+    final modelFromStringData = AstronomyPictureApiModel.fromStringData(
+      dateIso8601: date,
+      title: title,
+      explanation: explanation,
+      mediumDefinitionUrl: url,
+      highDefinitionUrl: hdurl,
+    );
+    test('should be a subclass of AstronomyPicture entity', () async {
+      expect(modelFromStringData, isA<AstronomyPicture>());
+    });
+    test('should extract "date" from the input string', () async {
+      expect(modelFromStringData.date, equals(DateTime.parse(date)));
+    });
+    test('should extract "title" from the input string', () async {
+      expect(modelFromStringData.title, equals(title));
+    });
+    test('should extract "explanation" from the input string', () async {
+      expect(modelFromStringData.explanation, equals(explanation));
+    });
+    test('should extract "hdurl" from the input string', () async {
+      expect(modelFromStringData.highDefinitionUrl, equals(Uri.parse(hdurl)));
+    });
+    test('should extract "url" from the input string', () async {
+      expect(modelFromStringData.mediumDefinitionUrl, equals(Uri.parse(url)));
     });
   });
 }
